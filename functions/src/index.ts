444416ae34db585ece93940fc3d4224cb1fc92d9
Prince Ita <princeIta@outlook.com>
2021-05-24 21:02:09 +0100

    export https functions trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,5 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import requestApi from './request-api';
+
+export const task = functions.https.onRequest(requestApi);
